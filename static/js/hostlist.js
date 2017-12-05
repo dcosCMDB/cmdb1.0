153c791938a21d0d808e5de428e2539edfab8c03
blingyxq <3130102248@zju.edu.cn>
2017-12-05 15:29:17 +0800

    test tablesorter

--- static/js/hostlist.js
+++ static/js/hostlist.js
@@ -22,7 +22,7 @@ $.get("/searchhost", function (ret) {
 				'<td>'+mem+'</td>'+
 				'<td>'+filesys+'</td>'+
 				'</tr>'
-        addrow(context)
+        $('#hosttbody').append(context)
 	}
 	addline="<tr class='info'>"+
          	"<td colspan='7'><a href='#' onclick='test()'><span class='glyphicon glyphicon-plus'></span></a></td>"+
