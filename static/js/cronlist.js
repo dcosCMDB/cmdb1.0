6dc2ff890cea15f4d8a432cda5b1e575db89a1cc
blingyxq <3130102248@zju.edu.cn>
2017-12-06 10:17:12 +0800

    test bootstrap table

--- static/js/cronlist.js
+++ static/js/cronlist.js
@@ -1,2 +1,60 @@
 /*init*/
+function newtable(cronres){
+$('#crontable').bootstrapTable({
+    search: true,  //是否显示搜索框功能
+    pagination: true,  //是否分页
+    showRefresh: true, //是否显示刷新功能
+    iconSize: 'outline',
+   // toolbar: '#exampleTableEventsToolbar', 可以在table上方显示的一条工具栏，
+    icons: {
+      refresh: 'glyphicon-repeat',
+      toggle: 'glyphicon-list-alt'
+    },
+  columns: [{
+      field: 'hostip',
+      title: 'ip',
+      sortable: true
+  }, {
+      field: 'hostname',
+      title: '主机名',
+      sortable: true
+  }, {
+      field: 'env',
+      title: '区域',
+      sortable: true
+  }, {
+      field: 'cpu',
+      title: 'CPU',
+      sortable: true
+  }, {
+      field: 'mem',
+      title: '内存',
+      sortable: true
+  }, {
+      field: 'filesys',
+      title: '文件系统'
+  },{
+      title: '查看脚本', field: 'cat',
+        formatter: function (val, row, idx) {
+          return '<button onclick="cat(this)""></button>';
+        }
+  }],
+  data: cronres
+});
+}
 $('#cronlistbtn').attr("style","color:#337ab7");
+$.get("/searchcron", function (ret) {
+	cronlist=ret.cronlist
+  newtable(cronlist)
+	}
+)
+
+function cat(obj){
+	hostip="1.1.1.1"
+	path="/data"
+	$.get("/cat", function (ret) {
+		text=ret.text
+		console.log(text)
+	}
+	)
+}
